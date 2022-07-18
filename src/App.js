9dd0c41655ec5f400f88599eb697eeb17fe89422
Jay Shah <shahjay1088@gmail.com>
2022-07-18 23:19:26 +0530

    basic react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import Navbar from "./componant/navbar";
+import './App.css'
+import data from "./data.js";
+import Card from "./componant/card"; 
+import Hero from "./componant/hero"
 
-function App() {
+
+export default function App(){
+
+  const cards= data.map(item => {
+    return (
+     <Card 
+     key={item.id}
+     {...item}
+     />
+  
+      )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>     
+      <Hero/>
+      <section className="cards-list">
+                {cards}
+            </section>
     </div>
-  );
+    
+  )
 }
-
-export default App;
